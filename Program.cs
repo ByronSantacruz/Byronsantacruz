7ae7a8490bfeacc874ae06befe5bd7a44a66cfc8
ByronSantacruz <94813891+ByronSantacruz@users.noreply.github.com>
2022-01-21 22:07:05 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Singletonp singletonp = Singletonp.Obteninstancia();
+            singletonp.datoss("Ingeniero en TI", 8);
+            Console.WriteLine(singletonp);
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+    }
+    
+}
---
+++ Singletonp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singletonp
+    {
+         private static Singletonp instancia;
+         private string profesion;
+         private int añolaboral;
+         private Singletonp() 
+         {
+            profesion = " ";
+            añolaboral = 12;
+         }
+         public static Singletonp Obteninstancia()
+         {
+            if (instancia == null)
+            {
+                instancia = new Singletonp();
+                Console.WriteLine("Instancia nueva");
+            }
+            return instancia;
+         }
+        public override string ToString()
+        {
+            return string.Format("La profesion es {0} y el año laboral es {1}", profesion, añolaboral);
+        }
+        public void datoss(string dprofesion, int dañolaboral)
+        {
+            profesion = dprofesion;
+            añolaboral = dañolaboral;
+        }
+    }
+}
